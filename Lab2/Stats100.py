dd0e0e8aa4cdfaf5c70ac14f1bd9bb79476be9b1
Sean <38899411+SeanKelly7@users.noreply.github.com>
2019-09-03 20:35:50 -0400

    Update Stats100.py

--- Lab2/Stats100.py
+++ Lab2/Stats100.py
@@ -1,7 +1,19 @@
 2.8
-
+def First100EvenSum(num):
+...     for i in range(2, 201, 2):
+...             i = i + i
+...     print(i)
+Im not sure if thats how you wanted it but i was able to get it to work fine but you need to put in a random number for num
 
 2.9
+def First50OddSum(num):
+...     for i in range (1, 100, 2):
+...             i = i + i
+...     print(i)
 
-
-
+2.10
+def First100Avg(num):
+...     for i in range(1, 200, 2):
+...             i = i + i
+...     avg = i / 100
+...     print(avg)
