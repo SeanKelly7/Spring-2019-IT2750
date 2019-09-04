6905a6b5d87181b6f2b13ab8ef7f128c0fa19b5b
Sean <38899411+SeanKelly7@users.noreply.github.com>
2019-09-03 22:03:33 -0400

    Update boolean.py

--- Lab2/boolean.py
+++ Lab2/boolean.py
@@ -2,18 +2,5 @@
 ...     print(count>= 1 and count<=10)
 ...     print(count)
 ...
->>> bool1(1)
-True
-1
->>> bool1(5)
-True
-5
->>> bool1(9)
-True
-9
->>> bool1(10)
-True
-10
->>> bool1(12)
-False
-12
+
+This question to me was a bit hard to read so i hope this came out the way it was supposed to
