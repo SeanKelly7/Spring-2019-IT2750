8406c23f5fa8a9d4446feb51f046c0dbfc6bec90
Sean <38899411+SeanKelly7@users.noreply.github.com>
2019-09-03 21:07:48 -0400

    Create boolean.py

---
+++ Lab2/boolean.py
@@ -0,0 +1,19 @@
+>>> def bool1(count):
+...     print(count>= 1 and count<=10)
+...     print(count)
+...
+>>> bool1(1)
+True
+1
+>>> bool1(5)
+True
+5
+>>> bool1(9)
+True
+9
+>>> bool1(10)
+True
+10
+>>> bool1(12)
+False
+12
