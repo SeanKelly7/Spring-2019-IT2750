9bdd0d8ca679db5029f5398c437a8678df3f5a02
Sean <38899411+SeanKelly7@users.noreply.github.com>
2019-09-03 21:58:40 -0400

    Create ifelse.py

---
+++ Lab2/ifelse.py
@@ -0,0 +1,17 @@
+def grade(score):
+    if score>=90:
+            gp=4
+             print(gp)
+    if score>=80 and score<=89:
+            gp=3
+            print(gp)
+    if score>=70 and score<=79:
+            gp=2
+            print(gp)
+    if score>=60 and score<=69:
+            gp=1
+            print(gp)
+    else:
+            if score<=59
+                    gp=0
+                    print(gp)
