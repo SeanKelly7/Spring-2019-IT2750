45acf809f9d8a2a76226c1b3bee1633159dcf744
Sean <38899411+SeanKelly7@users.noreply.github.com>
2019-09-09 20:44:56 -0400

    Create testCrypto.py

---
+++ Lab3/testCrypto.py
@@ -0,0 +1,14 @@
+secretum('veni vidi vici')
+>>> 'eivd iivn iivc'
+veritas(secretum('veni vidi vici')
+>>> 'veni vidi vici'
+
+secretum('Experience is the teacher of all things')
+>>> 'xeinei h ece faltigEprec stetahro l hns'
+veritas(secretum('Experience is the teacher of all things')
+>>> 'Experience is the teacher of all things'
+
+secretum('If you must break the law, do it to seize power: in all other cases observe it')
+>>> 'fyums ra h a,d tt ez oe:i l te ae bev tI o utbektelw oi osiepwr nalohrcssosrei'
+veritas(secretum(''If you must break the law, do it to seize power: in all other cases observe it')
+>>> 'If you must break the law, do it to seize power: in all other cases observe it'
