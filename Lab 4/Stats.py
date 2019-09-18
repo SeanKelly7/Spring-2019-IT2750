d1702bd964c01f75acff48a18433f9f29ea8416a
Sean <38899411+SeanKelly7@users.noreply.github.com>
2019-09-17 23:32:59 -0400

    Update Stats.py

--- Lab 4/Stats.py
+++ Lab 4/Stats.py
@@ -4,17 +4,37 @@
 ...
 >>> mean([34, 23, 54, 43, 53, 43, 99, 43, 74, 57, 82, 80, 17])
 
->>> def median(alist):
-...     copylist = alist[:] # make a copy using slice operator
-...     copylist.sort()
-...     if len(copylist)%2 == 0: # even length
-...         rightmid = len(copylist)//2
+>>> def median(medianlist):
+...     copy = medianlist[:] # make a copy using slice operator
+...     copy.sort()
+...     if len(copy)%2 == 0: # even length
+...         rightmid = len(copy)//2
 ...         leftmid = rightmid - 1
-...         median = (copylist[leftmid] + copylist[rightmid])/2
+...         median = (copy[leftmid] + copy[rightmid])/2
 ...     else:    #odd length
-...         mid = len(copylist)//2
-...         median = copylist [mid]
+...         mid = len(copy)//2
+...         median = copy [mid]
 ...     return median
 ...
 >>> median([34, 23, 54, 43, 53, 43, 99, 43, 74, 57, 82, 80, 17])
 
+>>> def mode(modelist):
+...     counts = {}
+...
+...     for item in modelist:
+...         if item in counts:
+...             counts[item] = counts[item]+1
+...         else:
+...             counts[item] = 1
+...
+...     countlist = counts.values()
+...     maxcount = max(countlist)
+...
+...     modelist = []
+...     for item in counts:
+...         if counts[item] == maxcount:
+...             modelist.append(item)
+...
+...     return modelist
+...
+>>> mode([34, 23, 54, 43, 53, 43, 99, 43, 74, 57, 82, 80, 17])
