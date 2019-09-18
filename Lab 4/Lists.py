a4d1f822117f85139a21ef33346386832f1351ce
Sean <38899411+SeanKelly7@users.noreply.github.com>
2019-09-18 11:08:32 -0400

    Update Lists.py

--- Lab 4/Lists.py
+++ Lab 4/Lists.py
@@ -1,5 +1,26 @@
-1. >>> list(string)
-['T', 'h', 'e', ' ', 'q', 'u', 'i', 'c', 'k', ' ', 'b', 'r', 'o', 'w', 'n', ' ', 'f', 'o', 'x', ' ', 'j', 'u', 'm', 'p', 's', ' ', 'o', 'v', 'e', 'r', ' ', 't', 'h', 'e', ' ', 'l', 'a', 'z', 'y', ' ', 'd', 'o', 'g', '.']
->>> [x.lower() for x in string]
+1.>>>string = "The quick brown fox jumps over the lazy dog"
+>>>list(string)
+>>>string = [x.lower() for x in string]
 
-2. 
+2. >>>string.sort()
+>>>string
+
+3.string = [x for x in string if x != " "]
+>>> def mode(stringlist):
+...     countdict = {}
+...     for ch in stringlist:
+...         if ch in countdict:
+...             countdict[ch] = countdict[ch] + 1
+...         else:
+...             countdict[ch] = 1
+...     countlist = countdict.values()
+...     maxcount = max(countlist)
+...     modelist = []
+...     for ch in countdict:
+...         if countdict[ch] == maxcount:
+...             modelist.append(ch)
+...     return modelist
+...
+>>> mode(string)
+
+4.
